eeefbc448b5c9e938534cb1e0b51c424d3309ec7
Trisha Wang <trish.regine@gmail.com>
2023-02-24 15:59:01 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import MovieList from "./components/MovieList";
+import react, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=toy&apikey=3b69450b";
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='movie-app container-fluid'>
+      <div className='movie-header'>
+        <div className='header'>
+          <img alt='movie-app-logo' src='../img/movie-app-logo.png' />
+          <input type='text' />
+        </div>
+      </div>
+      {/* movie body */}
+
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
