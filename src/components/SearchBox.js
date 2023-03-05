dc4140bb6705aabe950ef0c212cbc6130e808aec
Trisha Wang <trish.regine@gmail.com>
2023-03-05 18:51:04 +0800

    updated heading

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBox = (props) => {
   return (
-    <div className='col col-sm-3'>
+    <div className='col col-lg-3'>
       <input
         className='form-control shadow-none'
         value={props.value}
